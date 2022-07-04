1ee9c0c31a6ce169c3eaf303cf2d6e2195fc0674
CruelBlade <redeptikon@mail.ru>
2022-07-05 01:30:41 +0300

    lesson6

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -8,9 +8,15 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        TestBean testBean = context.getBean("testBean", TestBean.class);
+//        Music music = context.getBean("musicBean", Music.class);
+//
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
 
-        System.out.println(testBean.getName());
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
